6d51f5b3d39a0cc53c170c4cc5f90f81976934f6
spirpt1234 <1538110213@qq.com>
2019-01-16 12:29:02 +0800

    2019.1.16

--- python_work/第七章  用户输入和while循环/猜数游戏.py
+++ python_work/第七章  用户输入和while循环/猜数游戏.py
@@ -3,6 +3,7 @@
 import random
 a = int(random.random() * 100)
 b = True
+score = 100
 print('Let\'s play a game, you input a integer number, and I\'ll tell you bigger or smaller')
 while b:
     try:
@@ -12,13 +13,17 @@ while b:
     else:
         if c > a:
             print('That\'s bigger')
+            score -= 1
         elif c == a:
             print('So good, you are right')
+            print('Your score: ' + str(score))
             print('\nWould you want another round?')
             d = input('Please input Yes/No: ')
             if d == 'Yes' or d == 'Yes'.lower() or d == 'Yes'.upper():
                 b = True
+                score = 100
             else:
                 b = False
         else:
+            score -= 1
             print('That\'s smaller')
